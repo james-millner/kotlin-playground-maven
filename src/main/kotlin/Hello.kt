cf0e9c9317d223db39c6d7510bec33ba21db5f8f
James Millner <jamesmillnerdev@gmail.com>
2018-11-28 22:13:34 +0000

    Need to figure out how to import khttp to use in main kotlin class.

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -6,5 +6,11 @@ fun getHelloString() : String {
 
 fun main(args : Array<String>) {
     println(getHelloString())
+
+    var r = get("http://www.google.co.uk")
+    println(r.statusCode)
+    println(r.headers["content-type"])
+    println(r.text)
+    println(r.jsonObject)
 }
 
