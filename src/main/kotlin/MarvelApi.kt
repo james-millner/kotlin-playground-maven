9f70367813ad2f4fa3cc07679fdf56c947eb4017
James Millner <jamesmillnerdev@gmail.com>
2018-12-02 14:24:24 +0000

    Further code cleanup.

--- src/main/kotlin/MarvelApi.kt
+++ src/main/kotlin/MarvelApi.kt
@@ -7,28 +7,19 @@ import java.time.Instant
 
 fun main(args : Array<String>) {
 
-    var pubKey = args.get(0)
-    var privateKey = args.get(1)
+    val pubKey = args.get(0)
+    val privateKey = args.get(1)
 
     val characterFullURL = getURL("/characters", pubKey, privateKey, Instant.now());
 
-    var r = get(characterFullURL, buildHeaders())
+    val r = get(characterFullURL, buildHeaders())
     println(r.statusCode)
 
     if(r.statusCode == 200) {
 
         val marvelResponse = translateResponse(r.text, MarvelResponse.MarvelCharacterResponse::class.java)
 
-        if (marvelResponse != null) {
-            println(marvelResponse.attributionText)
-
-            for (result in marvelResponse.data.results) {
-                println(result.name)
-                for (u in result.urls) {
-                    println(u.url)
-                }
-            }
-        }
+        println(marvelResponse.toString())
     } else {
         println("Error: " + r.statusCode)
     }
