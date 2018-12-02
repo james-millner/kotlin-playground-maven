5f89b03682915bc2bfd1f0a5d752683206f760b3
James Millner <jamesmillnerdev@gmail.com>
2018-12-02 11:30:55 +0000

    Early project start stage.

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -6,10 +6,5 @@ fun getHelloString() : String {
 
 fun main(args : Array<String>) {
     println(getHelloString())
-
-    var r = khttp.get("http://www.google.co.uk")
-    println(r.statusCode)
-    println(r.headers["content-type"])
-    println(r.text)
 }
 
